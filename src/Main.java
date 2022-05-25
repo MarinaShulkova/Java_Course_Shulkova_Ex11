103abaeaf9743290f6bd2c77a69326eed2d6c247
samidster <samidster@gmail.com>
2022-05-25 17:24:14 +0300

    start

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder string = new StringBuilder();
+        string.insert(0,"Ivanof Ivan 89033234567 ivanov@mail.ru Moscow");
+        string.append("Petrof Petr +79029874532 petrov@gmail.com Kazan");
+        string.append("Dronof Dron +375908880033 dronov_dron@yandex.by Minsk");
+
+        Pattern pattern;
+    }
+}
