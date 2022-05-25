a809b0fe984a8b8e65c5f8377c9ab77eba4252cc
samidster <samidster@gmail.com>
2022-05-25 18:01:20 +0300

    start

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         StringBuilder string = new StringBuilder();
-        string.insert(0,"Ivanof Ivan 89033234567 ivanov@mail.ru Moscow");
-        string.append("Petrof Petr +79029874532 petrov@gmail.com Kazan");
-        string.append("Dronof Dron +375908880033 dronov_dron@yandex.by Minsk");
+        string.insert(0,"Ivanof Ivan 89033234567 ivanov@mail.ru Moscow \n");
+        string.append("Petrof Petr +79029874532 petrov@gmail.com Kazan \n");
+        string.append("Dronof Dron +375908880033 dronov_dron@yandex.by Minsk ");
 
-        Pattern pattern;
+        Pattern p_phone = Pattern.compile("\s\\d{10,}\s");
+        Pattern p_fio = Pattern.compile("[a-zA-Z]+");
+
+        Matcher m_phone = p_phone.matcher(string);
+        Matcher m_fio = p_fio.matcher(string);
+
+        while (m_phone.find()) System.out.println(m_phone.group());
+        while (m_fio.find()) System.out.println(m_fio.group());
+
     }
 }
