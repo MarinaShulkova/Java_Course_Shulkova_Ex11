6af335a8ac6354a5227cab04a9eae18decd93474
samidster <samidster@gmail.com>
2022-05-27 00:00:39 +0300

    27/05/22

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,30 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         StringBuilder string = new StringBuilder();
-        string.insert(0,"Ivanof Ivan 89033234567 ivanov@mail.ru Moscow \n");
-        string.append("Petrof Petr +79029874532 petrov@gmail.com Kazan \n");
-        string.append("Dronof Dron +375908880033 dronov_dron@yandex.by Minsk ");
+        string.insert(0,"|Ivanov Ivan|+79033234567|ivanov@mail.ru|Moscow/");
+        string.append("|Petrov Petr|89029874532|petrov@gmail.com|Kazan/");
+        string.append("|Sidorov Sidor|+375908880033|sidro@yandex.by|Minsk/");
 
-        Pattern p_phone = Pattern.compile("\s\\d{10,}\s");
-        Pattern p_fio = Pattern.compile("[a-zA-Z]+");
+        System.out.println(string);
+
+        Pattern p_phone = Pattern.compile("\\+?\\d{10,}");
+        Pattern p_fio = Pattern.compile("[a-zA-Z]+\\s[a-zA-Z]+");
+        Pattern p_email = Pattern.compile("[a-zA-Z]+[@][a-z.]+");
+        Pattern p_city = Pattern.compile("[a-zA-Z]+[/]");
+
 
         Matcher m_phone = p_phone.matcher(string);
         Matcher m_fio = p_fio.matcher(string);
+        Matcher m_email = p_email.matcher(string);
+        Matcher m_city = p_city.matcher(string);
 
-        while (m_phone.find()) System.out.println(m_phone.group());
+        while (m_phone.find()){
+            if (m_phone.group().charAt(0)=="8") m_phone.group().replace(0,2,"+7");
+        }
         while (m_fio.find()) System.out.println(m_fio.group());
+        while (m_email.find()) System.out.println(m_email.group());
+        while (m_city.find()) System.out.println(m_city.group());
+
 
     }
 }
